8e75c14c79a09f520ad5231d69ba492baea19a6a
enigma.ade <enigma.ade@bankmandiri.co.id>
2023-09-19 13:21:07 +0700

    Git init

--- src/main/java/com/project/training/microservices/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.training.microservices.repository;
-
-import com.project.training.microservices.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
